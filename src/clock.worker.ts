0ccef514fb36992b2157e832d3786e16e402146e
krokosik <krokosik@pm.me>
2026-01-04 13:48:40 +0100

    Use setInterval instead of requestAnimationFrame

--- src/clock.worker.ts
+++ src/clock.worker.ts
@@ -1,46 +1,26 @@
 let running: boolean = false
-let start: number | undefined
-let startTime: number | undefined
-let interval: number
-let prev: number | undefined
+let timerId: ReturnType<typeof setInterval> | undefined
 let lowFps: boolean = false
 
-const tick = (t: number): void => {
-    if (running) {
-        if (start === undefined) {
-            start = t
-            prev = t
-            requestAnimationFrame(tick)
-        } else {
-            interval += t - prev!
-            if (lowFps) {
-                if (interval >= 1000) {
-                    self.postMessage(interval)
-                    interval = 0
-                }
-            } else {
-                self.postMessage(t - prev!)
-            }
-            requestAnimationFrame(tick)
-
-            prev = t
-        }
-    }
-}
+// Tick interval in milliseconds
+const TICK_INTERVAL = 100
+const LOW_FPS_INTERVAL = 1000
 
 self.onmessage = async ({ data }) => {
     if (data.start) {
         lowFps = data.lowFps
         if (!running) {
             running = true
-            interval = 0
-            startTime = new Date().getTime()
-            requestAnimationFrame(tick)
+            const interval = lowFps ? LOW_FPS_INTERVAL : TICK_INTERVAL
+            timerId = setInterval(() => {
+                self.postMessage(interval)
+            }, interval)
         }
     } else {
         running = false
-        start = undefined
-        startTime = undefined
-        prev = undefined
+        if (timerId !== undefined) {
+            clearInterval(timerId)
+            timerId = undefined
+        }
     }
 }
